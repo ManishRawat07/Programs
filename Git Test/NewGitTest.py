68bd980a72830fac15ad6ffabe7816fd5173b044
ManishRawat07 <manish.rawat0702@gmail.com>
2023-02-03 11:58:53 -0600

    New Feature1

--- Git Test/NewGitTest.py
+++ Git Test/NewGitTest.py
@@ -2,4 +2,8 @@ def test_fun_git(x):
     print("This is test if codes are been published to github with proper commit")
     print(x)
 
+def new_features():
+    print("my new features is to display message")
+
 test_fun_git("This is commit")
+new_features("This is commit")
