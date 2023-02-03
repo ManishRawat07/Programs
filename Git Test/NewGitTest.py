1580603c51a747cad0b5d9e58431c924f4a9b639
ManishRawat07 <manish.rawat0702@gmail.com>
2023-02-03 12:05:43 -0600

    my first brancg commit

--- Git Test/NewGitTest.py
+++ Git Test/NewGitTest.py
@@ -1,9 +1,10 @@
 def test_fun_git(x):
     print("This is test if codes are been published to github with proper commit")
-    print(x)
 
-def new_features():
+
+def new_features(y):
     print("my new features is to display message")
+    print(y)
 
-test_fun_git("This is commit")
+test_fun_git(1)
 new_features("This is commit")
